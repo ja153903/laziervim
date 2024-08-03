b02575592ccb6d1ceeeb37be9e7d1a19c3c3c61f
ja153903 <jaimeabbariao@gmail.com>
2024-08-02 22:21:37 -0400

    shutting up some typescript behavior

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,6 +3,11 @@ return {
     "folke/noice.nvim",
     config = function()
       require("noice").setup({
+        lsp = {
+          hover = {
+            silent = true,
+          },
+        },
         cmdline = {
           format = {
             search_down = {
