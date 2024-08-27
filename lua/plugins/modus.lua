1bfae4847ab91c3e6a839c079e6b0ca1e0a807dc
ja153903 <jaimeabbariao@gmail.com>
2024-08-27 11:11:50 -0400

    updating colorscheme

--- lua/plugins/modus.lua
+++ lua/plugins/modus.lua
@@ -4,14 +4,13 @@ return {
     priority = 1000,
     config = function()
       require("modus-themes").setup({
-        style = "auto",
         variant = "default",
         transparent = false,
         dim_inactive = false,
         hide_inactive_statusline = false,
-        style = {
-          comments = { italic = true },
-          keywords = { italic = true },
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
           functions = {},
           variables = {},
         },
