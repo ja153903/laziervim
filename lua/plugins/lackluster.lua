10d6e7db820b8945f93eb2d216953a7d38f46cab
ja153903 <jaimeabbariao@gmail.com>
2024-09-03 22:22:00 -1000

    modifying bufferline

--- lua/plugins/lackluster.lua
+++ lua/plugins/lackluster.lua
@@ -2,4 +2,49 @@ return {
   "slugbyte/lackluster.nvim",
   lazy = false,
   priority = 1000,
+  config = function()
+    local lackluster = require("lackluster")
+
+    lackluster.setup({
+      tweak_ui = {
+        enable_end_of_buffer = true,
+      },
+      disable_plugin = {
+        bufferline = false,
+        cmp = false,
+        dashboard = false,
+        flash = false,
+        git_gutter = false,
+        git_signs = false,
+        headline = false,
+        lazy = false,
+        lightbulb = false,
+        lsp_config = false,
+        mason = false,
+        mini_diff = false,
+        navic = false,
+        noice = false,
+        notify = false,
+        oil = false,
+        rainbow_delimiter = false, -- if you want color-rainbows you should disable this
+        scollbar = false,
+        telescope = false,
+        todo_comments = false,
+        tree = false,
+        trouble = false,
+        which_key = false,
+        yanky = false,
+      },
+    })
+
+    require("nvim-web-devicons").setup({
+      color_icons = false,
+      override = {
+        ["default_icon"] = {
+          color = lackluster.color.gray4,
+          name = "Default",
+        },
+      },
+    })
+  end,
 }
