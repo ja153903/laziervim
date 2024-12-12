10562c5e967140fd21ea3db9f718751ceedb13ef
ja153903 <jaimeabbariao@gmail.com>
2024-12-11 23:02:57 -0500

    updating config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
   {
     "stevearc/oil.nvim",
+    enabled = false,
     opts = {
       -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
       -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
