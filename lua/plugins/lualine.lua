2e2d1dc45eef3ecfbc7553977c6381c95ede79d6
ja153903 <jaimeabbariao@gmail.com>
2024-12-27 18:17:17 -0500

    updating formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,20 +2,19 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
+      -- Bubbles config for lualine
+      -- Author: lokesh-krishna
+      -- MIT license, see LICENSE for more details.
 
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
+      local colors = {
+        blue = "#80a0ff",
+        cyan = "#79dac8",
+        black = "#080808",
+        white = "#c6c6c6",
+        red = "#ff5189",
+        violet = "#d183e8",
+        grey = "#303030",
+      }
 
       local bubbles_theme = {
         normal = {
